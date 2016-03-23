4fac9b5fac636f5d2d6810af685358aaae3af50f
Adam Buechler <butchler@gmail.com>
2016-03-22 20:36:04 -0400

    Add parsing for match details and match history apis.

--- generate-game-list.ts
+++ generate-game-list.ts
@@ -8,89 +8,6 @@ import { forEach } from 'lodash';
 // 8-4-4-4-12 format (.e.g e4e64922-2172-4099-b5b7-80dca6b47159).
 const uuid_pattern = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
 
-/*const gameSchema = {
-    type: 'object',
-    required: ['gameId', 'gameRealm'],
-    additionalProperties: true,
-    properties: {
-        gameId: {
-            // TODO: pattern
-            type: 'string'
-        },
-        gameRealm: {
-            // TODO: pattern
-            type: 'string'
-        }
-    }
-};
-const matchSchema = {
-    type: 'object',
-    required: ['id', 'games'],
-    additionalProperties: true,
-    properties: {
-        id: {
-            type: 'string',
-            pattern: uuid_pattern
-        },
-        games: {
-            type: 'object',
-            additionalProperties: false,
-            patternProperties: {
-                [uuid_pattern]: gameSchema
-            }
-        }
-    }
-};
-const bracketSchema = {
-    type: 'object',
-    required: ['matches'],
-    additionalProperties: true,
-    properties: {
-        matches: {
-            type: 'object',
-            // Each property must be a UUID.
-            additionalProperties: false,
-            patternProperties: {
-                [uuid_pattern]: matchSchema
-            }
-        }
-    }
-};
-const tournamentSchema = {
-    type: 'object',
-    required: ['id', 'brackets'],
-    additionalProperties: true,
-    properties: {
-        id: {
-            type: 'string',
-            pattern: uuid_pattern
-        },
-        brackets: {
-            type: 'object',
-            // Each property must be a UUID.
-            additionalProperties: false,
-            patternProperties: {
-                [uuid_pattern]: bracketSchema
-            }
-        }
-    }
-};
-const leagueInfoSchema = {
-    type: 'object',
-    required: ['highlanderTournaments'],
-    additionalProperties: true,
-    properties: {
-        highlanderTournaments: {
-            type: 'array',
-            minItems: 1,
-            uniqueItems: true,
-            items: tournamentSchema
-        }
-    },
-};
-
-const validateLeagueInfo = validator(leagueInfoSchema);*/
-
 const validateGame = validator({
     type: 'object',
     required: ['gameId', 'gameRealm'],
@@ -180,31 +97,90 @@ const validateLeague = validator({
     },
 });
 
+/*interface LeagueInfo {
+    matches: MatchInfo[];
+    games: Map<string, { gameId: string, gameRealm: string }>;
+}
+
 interface MatchInfo {
-    // TODO: Enforce UUID pattern.
+    tournamentId: string;
+    matchId: string;
+}
+
+interface League_GameInfo {
+    gameId: string;
+    gameRealm: string;
+}
+
+interface MatchDetails {
+    games: Array<MatchDetails_GameInfo>;
+}
+
+interface MatchDetails_GameInfo {
+    gameHash: string;
+    videoUrl: string;
+    teams: MatchDetails_TeamInfo[];
+}
+
+interface MatchDetails_TeamInfo {
+    name: string;
+    logoUrl: string;
+}
+
+interface MatchHistory_GameInfo {
+    startTimestamp: number;
+    durationSeconds: number;
+    teams: MatchHistory_TeamInfo[];
+}
+
+interface MatchHistory_TeamInfo {
+    numDeaths: number;
+    gold: number;
+}*/
+
+interface LeagueInfo {
+    matches: Array<MatchInfo>;
+    games: Map<string, GameInfo>;
+}
+
+interface MatchInfo {
     tournamentId: string;
     matchId: string;
 }
 
 interface GameInfo {
-    gameId: string;
-    gameRealm: string;
+    // All 3 of these are needed to construct the match details URL, which is
+    // used to get game duration, game start time, team deaths, and team gold.
+    gameId?: string;
+    gameRealm?: string;
     gameHash?: string;
-    videoUrl?: string;
-    teams?: TeamInfo[];
+
+    // Maps locale to video URL.
+    videos?: Map<string, string>;
+
+    teams?: Array<TeamInfo>;
+
+    startTime?: Date;
+    durationSeconds?: number;
 }
 
 interface TeamInfo {
-    name: string;
-    logoUrl: string;
+    acronym?: string;
+    name?: string;
+    logoUrl?: string;
+
+    kills?: number;
+    deaths?: number;
+    assists?: number;
+    gold?: number;
 }
 
-interface LeagueInfo {
-    matches: MatchInfo[];
-    games: Map<string, GameInfo>;
-}
+function parseLeagueInfo(leagueInfo: any): LeagueInfo | void {
+    if (validateLeague(leagueInfo) !== true) {
+        console.error('Dropping invalid league:', validateLeague.errors);
+        return null;
+    }
 
-function parseLeagueInfo(leagueInfo: any): LeagueInfo {
     const games = new Map<string, GameInfo>();
     const matches = new Array<MatchInfo>();
     const league = {
@@ -212,14 +188,9 @@ function parseLeagueInfo(leagueInfo: any): LeagueInfo {
         games
     };
 
-    if (validateLeague(leagueInfo) !== true) {
-        console.error('Invalid league info', validateLeague.errors);
-        return league;
-    }
-
     forEach(leagueInfo.highlanderTournaments, (tournament) => {
         if (validateTournament(tournament) !== true) {
-            console.error('Invalid tournament info', validateTournament.errors);
+            console.error('Dropping invalid tournament:', validateTournament.errors);
             return;
         }
 
@@ -227,13 +198,13 @@ function parseLeagueInfo(leagueInfo: any): LeagueInfo {
 
         forEach(tournament.brackets, (bracket) => {
             if (validateBracket(bracket) !== true) {
-                console.error('Invalid bracket info', validateBracket.errors);
+                console.error('Dropping invalid bracket:', validateBracket.errors);
                 return;
             }
 
             forEach(bracket.matches, (match) => {
                 if (validateMatch(match) !== true) {
-                    console.error('Invalid match info', validateMatch.errors);
+                    console.error('Dropping invalid match:', validateMatch.errors);
                     return;
                 }
 
@@ -246,7 +217,7 @@ function parseLeagueInfo(leagueInfo: any): LeagueInfo {
 
                 forEach(match.games, (game, key) => {
                     if (validateGame(game) !== true) {
-                        console.error('Invalid game info', validateGame.errors);
+                        console.error('Dropping invalid game:', validateGame.errors);
                         return;
                     }
 
@@ -262,5 +233,190 @@ function parseLeagueInfo(leagueInfo: any): LeagueInfo {
     return league;
 }
 
+const validateMatchDetails = validator({
+    type: 'object',
+    required: ['gameIdMappings', 'teams', 'videos'],
+    additionalProperties: true,
+    properties: {
+        gameIdMappings: {
+            type: 'array',
+            minItems: 1,
+            uniqueItems: true,
+            items: {
+                type: 'object',
+                required: ['id', 'gameHash'],
+                additionalProperties: false,
+                properties: {
+                    id: {
+                        type: 'string',
+                        pattern: uuid_pattern
+                    },
+                    gameHash: {
+                        // TODO: pattern
+                        type: 'string'
+                    }
+                }
+            }
+        },
+        teams: {
+            type: 'array',
+            minItems: 2,
+            uniqueItems: true,
+            items: {
+                type: 'object',
+                required: ['acronym', 'name', 'logoUrl'],
+                additionalProperties: true,
+                properties: {
+                    acronym: { type: 'string' },
+                    name: { type: 'string' },
+                    logoUrl: {
+                        type: 'string',
+                        format: 'uri'
+                    }
+                }
+            }
+        },
+        videos: {
+            type: 'array',
+            minItems: 1,
+            uniqueItems: true,
+            items: {
+                type: 'object',
+                required: ['game', 'locale', 'source'],
+                additionalProperties: true,
+                properties: {
+                    game: {
+                        type: 'string',
+                        pattern: uuid_pattern
+                    },
+                    locale: { type: 'string' },
+                    source: {
+                        type: 'string',
+                        format: 'uri'
+                    }
+                }
+            }
+        }
+    }
+});
+
+function parseMatchDetails(matchDetails: any): Map<string, GameInfo> | void {
+    if (validateMatchDetails(matchDetails) !== true) {
+        console.error('Dropping invalid match:', validateMatchDetails.errors);
+        return null;
+    }
+
+    const games = new Map<string, GameInfo>();
+
+    // Extract team info, which will be the same for each game.
+    const teams = new Array<TeamInfo>();
+    forEach(matchDetails.teams, (team) => {
+        teams.push({
+            acronym: team.acronym,
+            name: team.name,
+            logoUrl: team.logoUrl
+        });
+    });
+
+    // Extract gameHash.
+    forEach(matchDetails.gameIdMappings, (game) => {
+        games.set(game.id, {
+            gameHash: game.gameHash,
+            // Videos map locale to video URL.
+            videos: new Map<string, string>(),
+            teams
+        });
+    });
+
+    // Extract video URLs.
+    forEach(matchDetails.videos, (video) => {
+        const game = games.get(video.game);
+
+        if (game) {
+            game.videos.set(video.locale, video.source);
+        } else {
+            console.error('Found video for non-existent game:', video);
+        }
+    });
+
+    return games;
+}
+
+const validateMatchHistory = validator({
+    type: 'object',
+    required: ['participants', 'gameDuration', 'gameCreation'],
+    additionalProperties: true,
+    properties: {
+        gameDuration: { type: 'number' },
+        gameCreation: { type: 'number' },
+        participants: {
+            type: 'array',
+            minItems: 1,
+            uniqueItems: true,
+            items: {
+                type: 'object',
+                required: ['stats'],
+                additionalProperties: true,
+                properties: {
+                    stats: {
+                        type: 'object',
+                        required: ['kills', 'deaths', 'assists', 'goldEarned'],
+                        additionalProperties: true,
+                        properties: {
+                            kills: { type: 'number' },
+                            deaths: { type: 'number' },
+                            assists: { type: 'number' },
+                            goldEarned: { type: 'number' }
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
+
+function parseMatchHistory(matchHistory: any): GameInfo | void {
+    if (validateMatchHistory(matchHistory) !== true) {
+        console.error('Dropping invalid match history:', validateMatchHistory.errors);
+        return null;
+    }
+
+    const teams = new Map<number, TeamInfo>();
+
+    forEach(matchHistory.participants, (participant) => {
+        const teamId = participant.teamId;
+
+        if (!teams.has(teamId)) {
+            teams.set(teamId, {
+                kills: 0,
+                deaths: 0,
+                assists: 0,
+                gold: 0
+            });
+        }
+
+        const team = teams.get(teamId);
+        team.kills += participant.stats.kills;
+        team.deaths += participant.stats.deaths;
+        team.assists += participant.stats.assists;
+        team.gold += participant.stats.goldEarned;
+    });
+
+    const teamList = new Array<TeamInfo>();
+    teams.forEach((team) => teamList.push(team));
+
+    return {
+        durationSeconds: matchHistory.gameDuration,
+        startTime: new Date(matchHistory.gameCreation),
+        teams: teamList
+    };
+}
+
 const leagueInfo = JSON.parse(fs.readFileSync('./na-lcs.json', { encoding: 'utf-8' }));
 console.log(parseLeagueInfo(leagueInfo));
+console.log('-----------------------------------------------------------');
+const matchDetails = JSON.parse(fs.readFileSync('./match-details.json', { encoding: 'utf-8' }));
+console.log(parseMatchDetails(matchDetails));
+console.log('-----------------------------------------------------------');
+const matchHistory = JSON.parse(fs.readFileSync('./match-history.json', { encoding: 'utf-8' }));
+console.log(parseMatchHistory(matchHistory));
